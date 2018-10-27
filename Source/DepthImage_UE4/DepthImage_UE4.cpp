f2e69a113eeda14153f653885349fc801abcda12
n1ckfg <nick@fox-gieg.com>
2018-10-27 00:37:57 -0400

    first

---
+++ Source/DepthImage_UE4/DepthImage_UE4.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DepthImage_UE4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DepthImage_UE4, "DepthImage_UE4" );
---
+++ Source/DepthImage_UE4/DepthImage_UE4GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DepthImage_UE4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEPTHIMAGE_UE4_API ADepthImage_UE4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
